34c48327af6cfc5df88a024ee564faa8ed8b6735
Guillaume BERTHELOT <gberthelot@numalis.com>
2024-05-13 17:26:20 +0200

    avancement normal

--- src/abstractModule.py
+++ src/abstractModule.py
@@ -17,6 +17,7 @@ class AbstractLinear(nn.Module):
                             x:torch.tensor,
                             x_true:torch.tensor,
                             device:torch.device=torch.device("cpu")):
+            
             lin=lin.to(device)
             x =x.unsqueeze(1).to(device)
             x_true=x_true.to(device)
@@ -25,23 +26,21 @@ class AbstractLinear(nn.Module):
             lin_abs_bias_null = copy.deepcopy(lin).to(device)
             lin_abs_bias_null[1].weight.data =torch.abs(lin[1].weight.data)
             lin_abs_bias_null[1].bias.data = torch.zeros_like(lin[1].bias.data)
-        
-            
-            
             
             with torch.no_grad():    
                 x_true = lin(x_true)
                 x_value=lin(x[0])
                 x_epsilon=lin_copy_bias_null(x[1:-1])
                 x_noise=lin_abs_bias_null(x[-1])
+
             del x
             x = torch.cat((x_value,x_epsilon,x_noise),dim=0)
+           
             del x_value,x_epsilon,x_noise,lin_abs_bias_null,lin_copy_bias_null,
             gc.collect()
+            
             x_min = x[0] - torch.sum(torch.abs(x[1:]),dim=0)
             x_max = x[0] + torch.sum(torch.abs(x[1:]),dim=0)
-            
-           
           
             return x,x_min,x_max,x_true
         
@@ -61,6 +60,7 @@ class AbstractLinear(nn.Module):
             conv_copy_bias_null.bias.data = torch.zeros_like(conv.bias.data)
             conv_abs_bias_null.weight.data = torch.abs(conv.weight.data)
             conv_abs_bias_null.bias.data = torch.zeros_like(conv.bias.data)
+
             with torch.no_grad():
             
                 x_true = conv(x_true)
@@ -80,17 +80,14 @@ class AbstractLinear(nn.Module):
 
 
 
-
+import numpy as np
 class AbstractReLU(nn.Module):
+    max_symbol = np.inf
+    recycling = 1
+
     def __init__(self,max_symbols:Union[int,bool]=False):
         super(AbstractReLU,self).__init__()
-
-        
-    global max_symbol
-    global recycling
    
-        
-
     @staticmethod
     def abstract_relu(x:torch.Tensor,
                       x_min:torch.Tensor,
@@ -102,7 +99,6 @@ class AbstractReLU(nn.Module):
  
         x_min = x[0] - torch.sum(torch.abs(x[1:]),dim=0)
         x_max = x[0] + torch.sum(torch.abs(x[1:]),dim=0)
-
       
         sgn_min = torch.sign(x_min)
         sgn_max = torch.sign(x_max)
@@ -110,15 +106,15 @@ class AbstractReLU(nn.Module):
 
         coef_approx_linear = x_max/(torch.abs(x_max)+torch.abs(x_min))
         coef_approx_linear = torch.where(torch.isnan(coef_approx_linear),torch.zeros_like(coef_approx_linear),coef_approx_linear)
+        
         bias_approx_linear = x_max*(1-coef_approx_linear)/2
         noise_approx_linear = torch.abs(bias_approx_linear)
         x_true  = nn.ReLU()(x_true)
-       
-
         
         mask_p = (sgn==0)
         mask_1 =(sgn==2) + (sgn==1)
         mask_0 = (sgn==-2)+(sgn==-1)
+
         x[0,mask_p]=(coef_approx_linear[mask_p]*x[0,mask_p]+bias_approx_linear[mask_p])
         x[0,mask_1]=x[0,mask_1]
         x[0,mask_0]=0
@@ -126,11 +122,14 @@ class AbstractReLU(nn.Module):
         x[1:-1,mask_p]=coef_approx_linear[mask_p]*x[1:-1,mask_p]
         x[1:-1,mask_1]=x[1:-1,mask_1]
         x[1:-1,mask_0]=0
+
         x[-1,mask_p]=noise_approx_linear[mask_p]+torch.abs(coef_approx_linear[mask_p])*x[-1,mask_p]
         x[-1,mask_1]=x[-1,mask_1]
         x[-1,mask_0]=0
+
         del sgn_min,sgn_max,sgn,coef_approx_linear,bias_approx_linear,noise_approx_linear, mask_0,mask_1,mask_p
         gc.collect()
+
         x_min = x[0] - torch.sum(torch.abs(x[1:]),dim=0)
         x_max = x[0] + torch.sum(torch.abs(x[1:]),dim=0)
         
@@ -138,6 +137,7 @@ class AbstractReLU(nn.Module):
             if len(x)-2+len(torch.where(x[-1].flatten()!=0)[0])>AbstractReLU.max_symbol:
                 recycle_symbols = AbstractReLU.recycling*(AbstractReLU.max_symbol - len(x)+2)
                 recycle_symbols = int(recycle_symbols)
+           
             else :
                 recycle_symbols = AbstractReLU.recycling*(len(torch.where(x[-1].flatten()!=0)[0]))
                 recycle_symbols = int(recycle_symbols)
@@ -148,14 +148,11 @@ class AbstractReLU(nn.Module):
                 new_eps_batch_shape = x[-1].expand(len(new_eps)+1,-1).shape
                 new_eps_batch = torch.zeros(new_eps_batch_shape).to(device)
                 new_eps_batch[index,new_eps]=x[-1][new_eps]
-
                 x[-1].flatten()[new_eps]=0
                 new_eps_batch[-1] =x[-1].flatten()
-                
-            
                 x=x[:-1]
-
                 x = torch.cat((x,new_eps_batch),dim=0)
+           
             else :
                     pass
            
@@ -187,19 +184,19 @@ class AbstractReLU(nn.Module):
         noise_approx_linear = torch.abs(bias_approx_linear)
 
         x_true  = nn.ReLU()(x_true)
-    
-      
-        
+
         mask_p = (sgn==0)
         mask_1 =(sgn==2) + (sgn==1)
         mask_0 = (sgn==-2)+(sgn==-1)
+        
         x[0,mask_p]=(coef_approx_linear[mask_p]*x[0,mask_p]+bias_approx_linear[mask_p])
         x[0,mask_1]=x[0,mask_1]
         x[0,mask_0]=0
-
+        
         x[1:-1,mask_p]=coef_approx_linear[mask_p]*x[1:-1,mask_p]
         x[1:-1,mask_1]=x[1:-1,mask_1]
         x[1:-1,mask_0]=0
+        
         x[-1,mask_p]=noise_approx_linear[mask_p]+torch.abs(coef_approx_linear[mask_p])*x[-1,mask_p]
         x[-1,mask_1]=x[-1,mask_1]
         x[-1,mask_0]=0
@@ -214,33 +211,26 @@ class AbstractReLU(nn.Module):
         
        
         if add_symbol:
-              
-            
-      
-
-
             if len(x)-2+len(torch.where(x[-1].flatten()!=0)[0])>AbstractReLU.max_symbol:
                 recycle_symbols = AbstractReLU.recycling*(AbstractReLU.max_symbol - len(x)+2)
                 recycle_symbols = int(recycle_symbols)
+            
             else :
                 recycle_symbols = AbstractReLU.recycling*(len(torch.where(x[-1].flatten()!=0)[0]))
                 recycle_symbols = int(recycle_symbols)
+            
             if recycle_symbols>0:
-
                 new_eps = torch.topk((x[-1].flatten()),recycle_symbols).indices.to(device)
-                
                 index = torch.arange(len(new_eps)).to(device)
                 new_eps_batch_shape = x[-1].flatten().expand(len(new_eps)+1,-1).shape
                 new_eps_batch = torch.zeros(new_eps_batch_shape).to(device)
                 new_eps_batch[index,new_eps]=x[-1].flatten()[new_eps]
-
                 x[-1].flatten()[new_eps]=0
                 new_eps_batch[-1] =x[-1].flatten()
                 new_eps_batch = new_eps_batch.reshape(x[-1].expand(len(new_eps)+1,-1,-1,-1).shape)
-            
                 x=x[:-1]
-
                 x = torch.cat((x,new_eps_batch),dim=0) 
+
             else : pass 
           
                     
@@ -249,45 +239,49 @@ class AbstractReLU(nn.Module):
 
 
 
-
 class AbstractMaxpool2D(nn.Module):
+    max_symbol = np.inf
+    recycling = 1
+    
     def __init__(self,max_symbols:Union[int,bool]=False):
        super(AbstractMaxpool2D,self).__init__()
-   
-   
-    global max_symbol
-    global recycling
     
-
+ 
     @staticmethod
     def abstract_maxpool2D(maxpool:nn.Module,
                            x:torch.tensor,
                            x_true:torch.tensor,add_symbol:bool=False,
                            device:torch.device=torch.device("cpu"))->Tuple[torch.Tensor, torch.Tensor, torch.Tensor ]:
+        
         maxpool = maxpool.to(device)
         kernel_size = maxpool.kernel_size
-
+        
         assert kernel_size==2,f"Maxpool2D kernel size {kernel_size}. A kernel size different of 2 is not supported"
-
+        
         stride = maxpool.stride
         padding = maxpool.padding
+
         dim_x =len(x[0])
         x_min = x[0] - torch.sum(torch.abs(x[1:]),dim=0)
         x_max = x[0] + torch.sum(torch.abs(x[1:]),dim=0)
         
         x_final =maxpool(x_true)
+
         conv_0 = nn.Conv2d(dim_x, dim_x, kernel_size=kernel_size, stride=stride, padding=padding,groups=dim_x)
         conv_1 = nn.Conv2d(dim_x, dim_x, kernel_size=kernel_size, stride=stride, padding=padding,groups=dim_x)
         conv_2 = nn.Conv2d(dim_x, dim_x, kernel_size=kernel_size, stride=stride, padding=padding,groups=dim_x)
         conv_3 = nn.Conv2d(dim_x, dim_x, kernel_size=kernel_size, stride=stride, padding=padding,groups=dim_x)
+        
         w_0 = torch.tensor([[[[1., -1.], [0, 0.]]]])
         w_1 = torch.tensor([[[[0., 1.], [0, 0.]]]])
         w_2 = torch.tensor([[[[0., 0.], [0., 1.]]]])
         w_3 = torch.tensor([[[[0., 0.], [1., 0.]]]])
+        
         w_0 = w_0.expand(dim_x,-1,-1,-1)
         w_1 = w_1.expand(dim_x,-1,-1,-1)
         w_2 = w_2.expand(dim_x,-1,-1,-1)
         w_3 = w_3.expand(dim_x,-1,-1,-1)
+        
         conv_0.weight.data = w_0
         conv_0.bias.data =  torch.zeros(dim_x)
         conv_1.weight.data = w_1
@@ -296,54 +290,62 @@ class AbstractMaxpool2D(nn.Module):
         conv_2.bias.data =  torch.zeros(dim_x)
         conv_3.weight.data = w_3
         conv_3.bias.data =  torch.zeros(dim_x)
-
        #max(a,b,c,d) = relu(relu(relu(a-b)+b)+c)+d)
-
-
+        
         x_result,x_min_result,x_max_result,x_true_result  = AbstractLinear.abstract_conv2D(conv_0,x,x_true,device=device)
         x_result,x_min_result,x_max_result,x_true_result = AbstractReLU.abstract_relu_conv2D(x_result,x_min_result,x_max_result,x_true_result,device=device)
         x_result_1,x_min_result_1,x_max_result_1,x_true_result_1  = AbstractLinear.abstract_conv2D(conv_1,x,x_true,device=device)
+        
         x_result = AbstractBasic.abstract_addition(x_result_1,x_result)
+        
         x_min_result += x_min_result_1
         x_max_result += x_max_result_1
         x_true_result += x_true_result_1
-    
         x_result_1,x_min_result_1,x_max_result_1,x_true_result_1  = AbstractLinear.abstract_conv2D(conv_2,x,x_true,device=device)
+        
         x_result_1 = AbstractBasic.abstract_substraction(x_result_1, x_result)
+        
         x_min_result_1 -= x_min_result
         x_max_result_1 -= x_max_result
         x_true_result_1 -= x_true_result
         x_result_2,x_min_result_2,x_max_result_2,x_true_result_2  = AbstractReLU.abstract_relu_conv2D(x_result_1,x_min_result_1,x_max_result_1,x_true_result_1,add_symbol=False,device=device)
+        
         x_result_2 = AbstractBasic.abstract_addition(x_result_2, x_result)
+        
         x_min_result_2 += x_min_result
         x_max_result_2 += x_max_result
         x_true_result_2 += x_true_result
-
-
         x_result_1,x_min_result_1,x_max_result_1,x_true_result_1  = AbstractLinear.abstract_conv2D(conv_3,x,x_true,device=device)
+        
         x_result_1 =AbstractBasic.abstract_substraction(x_result_1, x_result_2)
+        
         x_min_result_1 -= x_min_result_2
         x_max_result_1 -= x_max_result_2
         x_true_result_1 -= x_true_result_2
         x_result_3,x_min_result_3,x_max_result_3,x_true_result_3  = AbstractReLU.abstract_relu_conv2D(x_result_1,x_min_result_1,x_max_result_1,x_true_result_1,device=device)
+        
         x_result_3 = AbstractBasic.abstract_addition(x_result_3,x_result_2)
+        
         x_min_result_3 += x_min_result_2
         x_max_result_3 += x_max_result_2
         x_true_result_3 += x_true_result_2
         x= x_result_3
+        
         x_min = x_min_result_3
         x_max = x_max_result_3
+        
         x_true = x_final
-
+        
         if add_symbol:
-             
+            
             if len(x)-2+len(torch.where(x[-1].flatten()!=0)[0])>AbstractMaxpool2D.max_symbol:
                 recycle_symbols = AbstractMaxpool2D.recycling*(AbstractMaxpool2D.max_symbol - len(x)+2)
                 recycle_symbols = int(recycle_symbols)
+            
             else :
                 recycle_symbols = AbstractMaxpool2D.recycling*(len(torch.where(x[-1].flatten()!=0)[0]))
                 recycle_symbols = int(recycle_symbols)
-                
+            
             if recycle_symbols>0:
                 new_eps = torch.topk((x[-1].flatten()),recycle_symbols).indices.to(device)
                 index = torch.arange(len(new_eps)).to(device)
@@ -354,15 +356,11 @@ class AbstractMaxpool2D(nn.Module):
                 new_eps_batch_last[new_eps]=0
                 new_eps_batch[-1] = new_eps_batch_last
                 new_eps_batch = new_eps_batch.reshape(x[-1].expand(len(new_eps)+1,-1,-1,-1).shape)
-                
                 x=x[:-1]
-
                 x = torch.cat((x,new_eps_batch),dim=0) 
             
             else :
                 pass    
-                
-         
         
         return x,x_min,x_max,x_true
 
@@ -412,7 +410,7 @@ class AbstractBasic(nn.Module):
 class abstractAvgPool2D(nn.Module):
     def __init__(self):
         super(abstractAvgPool2D,self).__init__()
-
+        
     @staticmethod
     def abstract_AvgPool2D(avgPool2D: nn.Module, x: torch.tensor,x_true: torch.tensor, device: torch.device = torch.device("cpu")) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         x_true = avgPool2D(x_true)
---
+++ src/abstractNN.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import copy
+from typing import List, Union, Tuple
+"""
+This class aims to make the tools that will be used to generate an abstract neural network
+In that abstract Neural Network, weight and bias will be affine function. 
+The objectif is to experiment a new way of learning 
+--> 
+"""
+
+
+class AbstractWeight(nn.Module):
+    def __init__(self):
+        super(AbstractWeight, self).__init__()
+     
+    """This method aims to generate the output of an abstract layer. For each weights that are in the index, 
+    an affine expression is generated and passed to the nexte layer (output). 
+    This method is used inside a forward pass, before that layer, it is a concrete evaluation,
+    after it becomes an abstract evaluation
+    The choice of the indexes to be abstracted can be done by the user, or by the use of pruning algorithms.
+    ."""
+    @staticmethod
+    def generate_ztp_from_layer_and_indexes(fully_connected_layer: nn.Module, 
+                     index : torch.Tensor,
+                     alpha : torch.Tensor, 
+                     input : torch.Tensor,
+                     device = torch.device("cpu"))->Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]: 
+        weight_of_layer = fully_connected_layer[1].weight.data
+        bias_of_layer = fully_connected_layer[1].bias.data
+        intermediate_layer = copy.deepcopy(fully_connected_layer).to(device)
+       
+        index = index.to(device)
+        output=[]
+        print(f"input.shape: {input.shape}")
+        x = fully_connected_layer(input)
+        print(f"x.shape: {x.shape}")
+        output.append(x)
+        for i in range(1,len(alpha)+1):
+            weight_epsilon = torch.zeros_like(weight_of_layer).flatten()
+            weight_epsilon[index[i-1]]=alpha[i-1]
+            weight_epsilon = weight_epsilon.reshape(weight_of_layer.shape)
+            intermediate_layer[1].weight.data = weight_epsilon
+            abstract_tensor_layer = intermediate_layer(input)-intermediate_layer(torch.zeros_like(input))     
+
+            output.append(abstract_tensor_layer)
+        output.append(torch.zeros_like(x))
+        output = torch.stack(output).squeeze(1)
+
+        output_min = x[0] - torch.sum(torch.abs(x[1:]),dim=0)
+        output_max = x[0] + torch.sum(torch.abs(x[1:]),dim=0)
+        print(f"output.shape: {output.shape}")
+        return output,output_min,output_max, x
+       
