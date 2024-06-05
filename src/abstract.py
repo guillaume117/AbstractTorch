5a136ff8c2d7faadc3348621c61dae638f289783
Guillaume BERTHELOT <gberthelot@numalis.com>
2024-06-05 17:05:06 +0200

    ajout sparse

--- src/abstract.py
+++ src/abstract.py
@@ -29,3 +29,25 @@ class abstractTensor:
 
         
         return abstract_tensor
+    
+    def abstract_tensor_index(self,index):
+        #assert len(self.alpha)==len(self.tensor.flatten()), "The length of alpha should be equal to the length of the flatten tensor"
+       
+        
+        flatten_tensor  = self.tensor.flatten()
+     
+        abstract_tensor=[]
+        abstract_tensor.append(self.tensor)
+        for i in range(1,len(self.alpha)+1):
+           
+            abstract_tensor_layer = torch.zeros_like(flatten_tensor)
+        
+            abstract_tensor_layer[index[i-1]]=self.alpha[i-1]
+            abstract_tensor_layer = abstract_tensor_layer.reshape(self.tensor.shape)
+            abstract_tensor.append(abstract_tensor_layer)
+        
+        abstract_tensor.append(torch.zeros_like(self.tensor))
+        abstract_tensor= torch.stack(abstract_tensor)
+
+        
+        return abstract_tensor
